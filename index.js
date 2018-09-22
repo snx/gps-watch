aabe9218a79cdda179b4547d94b4982e6c0c4c10
snx <snxsnb@gmail.com>
2018-09-23 02:42:40 +0300

    showing results fixed

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-let watcherId;
-
 function showResults(data) {
     document.querySelector('#gps-results').textContent = JSON.stringify(data);
 }
 
 function watch() {
-    if (!watcherId) {
-        window.navigator.geolocation.watchPosition(
-            data => showResults(data),
-            error => showResults(error),
-            {
-                maximumAge: 0,
-                timeout: 60,
-            },
-        );
-    }
+    window.navigator.geolocation.watchPosition(
+        ({ coords: { latitude, longitude, accuracy } }) => showResults({ latitude, longitude, accuracy }),
+        ({ message }) => showResults({ error: message }),
+        {
+            maximumAge: 0,
+            timeout: 60000,
+        },
+    );
 }
